55e2d4231e306f47d2f778ab9eb4a73f128e933d
carmencapobianco <carmen.capobianco@gmail.com>
2018-05-09 16:50:38 +0200

    nocomment

---
+++ Algoritmi/Esercizio1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmi {
+    class Esercizio1 {
+        static void Main(string[] args) {
+            int n = 0;
+            int s = NumeriPari(n);
+            NumeriDispari(s);
+        }
+        public static int NumeriPari(int n){ 
+            for (int i=0; i<=14; i++){
+                n = n+2;
+                Console.WriteLine($"{n}");
+            }
+            return n;
+        }
+        public static void NumeriDispari(int s){ 
+            int r = s+1;
+            int a = s+1;
+            
+            int i = s+1;
+            for (i=a; r>=2; i--){
+                r = r-2;
+                Console.WriteLine($"{r}");
+            }
+        }
+    }
+}
---
+++ Algoritmi/Esercizio2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmi {
+    class Esercizio2 {
+        static void Main(string[] args) {
+            string frase = "ancora non ho fatto niente ancora non ho fatto niente ancora non ho fatto niente";
+            int conta = ContaParole(frase);
+        }
+        public static int ContaParole(string frase){ 
+            List<string> ElencoParole = new List<string>();
+            int conta = 0;
+            foreach(char c in frase){ 
+                if (c == ' ') conta++;
+                
+                conta = conta+2;
+                Console.WriteLine($"{frase}");
+                return conta;
+            }
+        }
+    }
+}
